bc11fbb011f26d07b5696fa7d1e8996cdac806f6
Hillbillyer <contact@hillbillyer.net>
2023-11-14 12:19:31 +1100

    Updated and now functional

--- lgsm/new-install.sh
+++ lgsm/new-install.sh
@@ -13,41 +13,49 @@ done <  <(tail -n +2 hills-scripts/lgsm/serverlist.csv)
 
 
 
-PS3='Please enter your choice: '
-select opt in "${servers[@]}";
+PS3='--Type "quit" to exit the Script--
+Please enter your choice: '
+select opt in "${servers[@]}" "quit";
 do
-    # Counter-Strike 2
-    if [ "$REPLY" == "Counter-Strike 2" ];
-    then
-    echo "Installing Dependencies"
-        sleep 3s
-        clear
-        sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd
-        sleep 3s
-        clear
-    echo "Installing Counter-Strike 2"
-        sleep 3s
-        clear
-        username=cs2
-        read -p "Choose a Password for cs2: " password
-        sudo adduser --gecos "cs2" --disabled-password $username
-        sudo chpasswd <<<"$username:$password"
-        su - $username -c "wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh cs2server"
-        su - $username -c "./cs2server auto-install"
-    fi
-    # case $opt in 
-    #     "Assetto Corsa")
-    #         echo "you chose choice 1"
-    #         ;;
-    #     "Option 2")
-    #         echo "you chose choice 2"
-    #         ;;
-    #     "Option 3")
-    #         echo "you chose choice $REPLY which is $opt"
-    #         ;;
-    #     "Quit")
-    #         break
-    #         ;;
-    #     *) echo "invalid option $REPLY";;
-    # esac
+    case $opt in 
+        "quit")
+            echo "Exiting the script."
+            break
+            ;;
+        *)
+            clear
+            echo " $opt <-- Selected
+            
+            "
+            # Add your custom actions based on the selected server here
+
+            echo "Installing Dependencies"
+            sleep 3s
+            clear
+                sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd speex:i386 libtbb2 -y
+            sleep 3s
+            clear
+            echo "Installing $opt"
+            sleep 3s
+            clear
+            username=${server_info["$opt"]}
+            read -p "Choose a Password for ${server_info["$opt"]}: " password
+            sudo adduser --gecos "${server_info["$opt"]}" --disabled-password $username
+            sudo chpasswd <<<"$username:$password"
+            su - $username -c "wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh '$username'server"
+            su - $username -c "./'$username'server update-lgsm"
+            clear
+            read -p "
+            Enter your Steam Username: 
+            
+            --This is for Game Servers that require Steam Authentication.--
+            --Feel free to press Enter and leave blank if unneccessary.-- 
+            " steamusername
+            clear
+            read -p "Enter your Steam Password: " steampassword
+            echo -e "steamuser=$steamusername\nsteampass=$steampassword" >> /home/"$username"/lgsm/config-lgsm/"$username"server/common.cfg
+            su - $username -c "./'$username'server auto-install"
+
+            ;;
+    esac
 done
