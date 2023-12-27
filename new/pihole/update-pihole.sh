a3ebe2a35a1c6dbc0f79f5472d5b8834e5d281e5
Hillbillyer <contact@hillbillyer.net>
2023-12-28 07:26:51 +1100

    Created script for pihole servers.

---
+++ new/pihole/update-pihole.sh
@@ -0,0 +1,34 @@
+# !/bin/sh
+
+#Machine Updates
+sudo /etc/scripts/new/update.sh
+
+## NTFY Notification Start
+curl -H "Title: $host Pihole Update" -H "Markdown: yes" -d "$host Pihole Update Initiated" https://ntfy.ahillier.dev/"$host"
+
+    # Pihole Update
+    sudo pihole -up
+
+## NTFY Notification End
+curl -H "Title: $host Pihole Update" -H "Markdown: yes" -d "$host Pihole Update Complete" https://ntfy.ahillier.dev/"$host"
+
+# Intellectual Property
+echo "Script By: "
+echo "
+ █████   █████ ███ ████ ████ █████      ███ ████ ████                             
+░░███   ░░███ ░░░ ░░███░░███░░███      ░░░ ░░███░░███                             
+ ░███    ░███ ████ ░███ ░███ ░███████  ████ ░███ ░███ █████ ████ ██████  ████████ 
+ ░███████████░░███ ░███ ░███ ░███░░███░░███ ░███ ░███░░███ ░███ ███░░███░░███░░███
+ ░███░░░░░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███░███████  ░███ ░░░ 
+ ░███    ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███░███░░░   ░███     
+ █████   ████████████████████████████  ███████████████░░███████░░██████  █████    
+░░░░░   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░  ░░░░░░░░░░░░░░░  ░░░░░███ ░░░░░░  ░░░░░     
+                                                       ███ ░███                   
+                                                      ░░██████                    
+                                                       ░░░░░░                     
+"
+echo "https://hillbillyer.net"
+echo "contact@hillbillyer.net"
+
+sleep 3s
+sudo reboot
