2c07636ac075a5f85c1656ecb8d709f876a1da0c
Hillbillyer <contact@hillbillyer.net>
2024-01-23 17:20:45 +1100

    Updated formatting and aliases

--- alias.sh
+++ alias.sh
@@ -5,7 +5,9 @@ sed -i '/##CUSTOM ALIASES/,/^$/d' ~/.bashrc
 echo "
 ##CUSTOM ALIASES
 alias update='sudo /hill/scripts/update.sh'
+alias install-lgsm='sudo /hill/scripts/linuxgsm/install-lgsm.sh'
 alias update-lgsm='sudo /hill/scripts/linuxgsm/update-lgsm.sh'
-alias start-all='sudo /hill/scripts/linuxgsm/start-lgsm.sh'
+alias start-lgsm='sudo /hill/scripts/linuxgsm/start-lgsm.sh'
+alias stop-lgsm='sudo /hill/scripts/linuxgsm/stop-lgsm.sh'
 alias update-pihole='sudo /hill/scripts/pihole/update-pihole.sh'
 " >> ~/.bashrc
