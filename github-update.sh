e034e8fb7a93afe9bbb2bff6296a2e45f48bac32
Hillbillyer <contact@hillbillyer.net>
2024-02-01 12:11:51 +1100

    added notification

--- github-update.sh
+++ github-update.sh
@@ -17,3 +17,14 @@ chmod +x /hill/scripts/linuxgsm/start-lgsm.sh
 chmod +x /hill/scripts/linuxgsm/restart-lgsm.sh
 chmod +x /hill/scripts/linuxgsm/stop-lgsm.sh
 chmod +x /hill/scripts/pihole/update-pihole.sh
+
+## Creating Host Variable
+clear
+    host=$(cat /etc/hostname)
+
+## Discord Notification Start
+export WEBHOOK_URL="https://discord.com/api/webhooks/1202410026367455292/lOo50UlEEdfr48-a1wdppo30HOQGIWtEb5D9POVxYknK9QuXiFdIm3zp0JRFtTYLqRF9"
+curl \
+  -H "Content-Type: application/json" \
+  -d '{"username": "Hacker Man", "content": "'$host' updated Github Repository"}' \
+  $WEBHOOK_URL
