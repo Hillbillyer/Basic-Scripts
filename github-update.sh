3d5248e7b734ed66c7259a4b0c25cb1184e36c7b
Hillbillyer <contact@hillbillyer.net>
2024-02-01 18:07:00 +1100

    Formatting

--- github-update.sh
+++ github-update.sh
@@ -17,13 +17,15 @@ chmod +x /hill/scripts/linuxgsm/restart-lgsm.sh
 chmod +x /hill/scripts/linuxgsm/stop-lgsm.sh
 chmod +x /hill/scripts/pihole/update-pihole.sh
 
-## Creating Host Variable
+## Creating Host Variable ##
 clear
-    host=$(cat /etc/hostname)
+host=$(cat /etc/hostname)
+## ---------------------------------------- ##
 
-## Discord Notification Start
+## Discord Notification ##
 export WEBHOOK_URL="https://discord.com/api/webhooks/1202410026367455292/lOo50UlEEdfr48-a1wdppo30HOQGIWtEb5D9POVxYknK9QuXiFdIm3zp0JRFtTYLqRF9"
 curl \
   -H "Content-Type: application/json" \
   -d '{"username": "Hacker Man", "content": "'$host' updated Github Repository"}' \
   $WEBHOOK_URL
+## ---------------------------------------- ##
--- ubuntu/cloud-update.sh
+++ ubuntu/cloud-update.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-## Creating Host Variable
+## Creating Host Variable ##
 clear
-    host=$(cat /etc/hostname)
+host=$(cat /etc/hostname)
+## ---------------------------------------- ##
 
-## Discord Notification Start
+## Discord Notification ##
 export WEBHOOK_URL="https://discord.com/api/webhooks/1202411890106564709/pbpcKw6XvUpVRDjmpr4QiGSfV8p-6VeqdE8nvWQfhPKfwUVWU3VYrPPvULM_VJJyuLia"
 curl \
   -H "Content-Type: application/json" \
   -d '{"username": "Hacker Man", "content": "'$host' has started Updating!"}' \
   $WEBHOOK_URL
+## ---------------------------------------- ##
 
-#Machine Updates
+#Machine Updates ##
 sudo /hill/scripts/update.sh
+## ---------------------------------------- ##
 
-## Discord Notification End
+## Discord Notification ##
 export WEBHOOK_URL="https://discord.com/api/webhooks/1202411890106564709/pbpcKw6XvUpVRDjmpr4QiGSfV8p-6VeqdE8nvWQfhPKfwUVWU3VYrPPvULM_VJJyuLia"
 curl \
   -H "Content-Type: application/json" \
   -d '{"username": "Hacker Man", "content": "'$host' has finished Updating!"}' \
   $WEBHOOK_URL
+## ---------------------------------------- ##
 
-# Intellectual Property
+## Intellectual Property ##
 echo "Script By: "
 echo "
  █████   █████ ███ ████ ████ █████      ███ ████ ████                             
@@ -38,6 +42,7 @@ echo "
 "
 echo "https://hillbillyer.net"
 echo "contact@hillbillyer.net"
+## ---------------------------------------- ##
 
 sleep 3s
 sudo reboot
