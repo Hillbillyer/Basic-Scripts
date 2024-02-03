4bcada6c83ec595e6980fe170d0c045b69814eec
Hillbillyer <contact@hillbillyer.net>
2024-02-03 23:23:15 +1100

    Add docker container creation

--- docker/server-1-docker.sh
+++ docker/server-1-docker.sh
@@ -3,13 +3,58 @@
 read -p "Container: " container
 read -p "Image: " image
 
-sudo docker stop "$container"
-# $(docker ps -a -q)
+## Stop All Docker Containers
+    sudo docker stop "$container"
+    # $(docker ps -a -q)
+    ## ---------------------------------------- ##
 
-sudo docker rm "$container"
-# $(docker ps -a -q)
+## Delete All Docker Containers
+    sudo docker rm "$container"
+    # $(docker ps -a -q)
+    ## ---------------------------------------- ##
 
-sudo docker rmi "$image-2"
-# $(docker images -a)
+## Delete All Docker Images
+    sudo docker rmi "$image"
+    # $(docker images -a)
+    ## ---------------------------------------- ##
 
-cd dashy && sudo docker-compose up -d
+### CONTAINER CREATION ###
+    ## Create Dashy Container
+        cd /home/andrew/dashy && sudo docker-compose up -d && cd 
+        ## ---------------------------------------- ##
+
+    ## Create Uptime Kuma Container
+        sudo ./home/andrew/kuma/kuma.sh
+        ## ---------------------------------------- ##
+
+    ## Create FreshRSS Container
+        sudo ./home/andrew/freshrss/freshrss.sh
+        ## ---------------------------------------- ##
+
+    ## Create NTFY Container
+        sudo ./home/andrew/ntfy/ntfy.sh
+        ## ---------------------------------------- ##
+
+    ## Create Kavita Container
+        sudo ./home/andrew/kavita/kavita.sh
+        ## ---------------------------------------- ##
+
+    ## Create Homebox Container
+        cd /home/andrew/homebox && sudo docker-compose up -d && cd
+        ## ---------------------------------------- ##
+
+    ## Create Freely Container
+        cd /home/andrew/freely && sudo docker-compose up -d && cd
+        ## ---------------------------------------- ##
+
+    ## Create Invidious Container
+        cd /home/andrew/invidious && sudo docker-compose up -d && cd
+        ## ---------------------------------------- ##
+
+    ## Create Trilium Container
+        cd /home/andrew/trilium && sudo docker-compose up -d && cd
+        ## ---------------------------------------- ##
+
+    ## Create Invoice Ninja Container
+        cd /home/andrew/ninja && sudo docker-compose up -d && cd
+        ## ---------------------------------------- ##
