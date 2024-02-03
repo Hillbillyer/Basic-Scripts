15bad001064aa47a8b94948d7d94ac535daf49e3
Hillbillyer <contact@hillbillyer.net>
2024-02-03 23:36:22 +1100

    fixed crap

--- github-update.sh
+++ github-update.sh
@@ -6,10 +6,10 @@ sudo chown andrew:andrew /hill
 sudo chgrp andrew /hill
 git clone https://github.com/Hillbillyer/scripts /hill/scripts
 
-chmod +x /hill/scripts/github-update.sh
 chmod +x /hill/scripts/update.sh
 chmod +x /hill/scripts/ubuntu/cloud-update.sh
 chmod +x /hill/scripts/ubuntu/local-update.sh
+chmod +x /hill/scripts/docker/server-1-docker.sh
 chmod +x /hill/scripts/linuxgsm/install-lgsm.sh
 chmod +x /hill/scripts/linuxgsm/update-lgsm.sh
 chmod +x /hill/scripts/linuxgsm/start-lgsm.sh
