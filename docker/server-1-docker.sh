7e4647caa4955f2dc27fa7aba161716ecc85b0b7
Hillbillyer <contact@hillbillyer.net>
2024-02-04 08:30:46 +1100

    Added NTFY notifications

--- docker/server-1-docker.sh
+++ docker/server-1-docker.sh
@@ -1,58 +1,78 @@
 #!/bin/bash
 
+## Creating Host Variable ##
+    clear
+    host=$(cat /etc/hostname)
+## ---------------------------------------- ##
+
 ## Stop All Docker Containers
     sudo docker stop $(docker ps -a -q)
-    ## ---------------------------------------- ##
+    curl -H "Title: $host docker" -d "Stopped All Docker Containers" https://ntfy.hillbillyer.dev/"$host"
+## ---------------------------------------- ##
 
 ## Delete All Docker Containers
     sudo docker rm $(docker ps -a -q)
-    ## ---------------------------------------- ##
+    curl -H "Title: $host docker" -d "Deleted All Docker Containers" https://ntfy.hillbillyer.dev/"$host"
+## ---------------------------------------- ##
 
 ## Delete All Docker Images
     sudo docker rmi $(docker images -a)
-    ## ---------------------------------------- ##
+    curl -H "Title: $host docker" -d "Deleted All Docker Images" https://ntfy.hillbillyer.dev/"$host"
+## ---------------------------------------- ##
 
 ### CONTAINER CREATION ###
 
     ## Create NGINX Container
+        curl -H "Title: $host docker" -d "Recreating NGINX Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/nginx && sudo docker-compose up -d && cd
     ## ---------------------------------------- ##
 
     ## Create Dashy Container
+        curl -H "Title: $host docker" -d "Recreating Dashy Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/dashy && sudo docker-compose up -d && cd 
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create Uptime Kuma Container
+        curl -H "Title: $host docker" -d "Recreating Uptime Kuma Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/kuma && sudo ./kuma.sh && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create FreshRSS Container
+        curl -H "Title: $host docker" -d "Recreating FreshRSS Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/freshrss && sudo ./freshrss.sh && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create NTFY Container
+        curl -H "Title: $host docker" -d "Recreating NTFY Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/ntfy && sudo ./ntfy.sh && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create Kavita Container
+        curl -H "Title: $host docker" -d "Recreating Kavita Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/kavita && sudo ./kavita.sh && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create Homebox Container
+        curl -H "Title: $host docker" -d "Recreating Homebox Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/homebox && sudo docker-compose up -d && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create Freely Container
+        curl -H "Title: $host docker" -d "Recreating Freely Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/freely && sudo docker-compose up -d && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create Invidious Container
+        curl -H "Title: $host docker" -d "Recreating Invidious Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/invidious && sudo docker-compose up -d && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
 
     ## Create Invoice Ninja Container
+        curl -H "Title: $host docker" -d "Recreating Invoice Ninja Container" https://ntfy.hillbillyer.dev/"$host"
         cd /home/andrew/ninja && sudo docker-compose up -d && cd
-        ## ---------------------------------------- ##
+    ## ---------------------------------------- ##
+
+curl -H "Title: $host docker" -d "All Containers Up & Running" https://ntfy.hillbillyer.dev/"$host"
 
 sleep 3s
 sudo reboot
